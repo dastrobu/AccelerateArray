69d8ff60ca38684566df0fe7c25de88986591d42
Daniel Strobusch <1847260+dastrobu@users.noreply.github.com>
2019-04-07 15:25:00 +0200

    removed @inlinable to support swift 4.

--- Sources/AccelerateArray/internal.swift
+++ Sources/AccelerateArray/internal.swift
@@ -2,7 +2,7 @@
 ///
 internal extension Array {
     /// dimension, if the array represents a square matrix
-    @inlinable var n: Int {
+    var n: Int {
         let n = Int(Double(count).squareRoot())
         assert(n * n == count, "\(n) * \(n) == \(count)")
         return n
