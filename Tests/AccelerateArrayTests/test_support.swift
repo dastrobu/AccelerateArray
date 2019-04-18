34e89e8c761bac8066051813fe41e66e1f81f3d4
Daniel Strobusch <1847260+dastrobu@users.noreply.github.com>
2019-04-18 20:56:38 +0200

    float support for lapack functions.

--- Tests/AccelerateArrayTests/test_support.swift
+++ Tests/AccelerateArrayTests/test_support.swift
@@ -5,10 +5,10 @@
 import XCTest
 
 /// assertion for arrays with accuracy
-public func XCTAssertEqual<T>(_ expression1: @autoclosure () throws -> [T],
-                              _ expression2: @autoclosure () throws -> [T],
-                              accuracy: T, _ message: @autoclosure () -> String = "",
-                              file: StaticString = #file, line: UInt = #line) rethrows where T: FloatingPoint {
+internal func XCTAssertEqual<T>(_ expression1: @autoclosure () throws -> [T],
+                                _ expression2: @autoclosure () throws -> [T],
+                                accuracy: T, _ message: @autoclosure () -> String = "",
+                                file: StaticString = #file, line: UInt = #line) rethrows where T: FloatingPoint {
     let array1: [T] = try expression1()
     let array2: [T] = try expression2()
     XCTAssertEqual(array1.count, array2.count, file: file, line: line)
