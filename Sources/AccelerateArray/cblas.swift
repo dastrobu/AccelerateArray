b5d291812202c07acbf5e65ee783852ddd317e66
Daniel Strobusch <1847260+dastrobu@users.noreply.github.com>
2020-01-13 19:53:34 +0100

    add axpby blas function. (#9)

--- Sources/AccelerateArray/cblas.swift
+++ Sources/AccelerateArray/cblas.swift
@@ -29,12 +29,53 @@ public extension Array where Element == Float {
 
     /// Modifies a vector in place, setting each element to a given value.
     /// https://developer.apple.com/documentation/accelerate/blas
+    ///
+    /// - Parameters:
+    ///     - n: number of items
+    ///     - alpha: scaling factor
+    ///     - incX: stride within x
+    ///     - offset: offset w.r.t to the start of the array from which to start the operation
     mutating func set(n: Int32, alpha: Element, incX: Int32 = 1, offset: Int = 0) {
         assert(offset == 0 || (offset >= 0 && offset < count),
             "\(offset) == 0 || (\(offset) >= 0 && \(offset) < \(count))")
-        assert(n >= 0 && n <= count - offset, "\(n) >= 0 && \(n) <= \(count) - \(offset)")
+        assert(n >= 0 && n / incX <= count - offset, "\(n) >= 0 && \(n) / \(incX) <= \(count) - \(offset)")
         assert(incX >= 1, "\(incX) >= 1")
         catlas_sset(n, alpha, &self + offset, incX)
+    }
+
+    /// Computes the sum of two vectors, scaling each one separately (single-precision).
+    /// On return, the contents of vector Y are replaced with the result.
+    /// https://developer.apple.com/documentation/accelerate/blas
+    /// - Parameters:
+    ///     - alpha: scaling factor x
+    ///     - beta: scaling factor for y
+    ///     - offsetX: offset w.r.t to the start of the array from which to start the operation in x
+    ///     - offsetY: offset w.r.t to the start of the array from which to start the operation in y
+    mutating func axpby(alpha: Element, beta: Element, y: inout [Element], offsetX: Int = 0, offsetY: Int = 0) {
+        axpby(n: Int32(count - offsetX), alpha: alpha, beta: beta, y: &y, offsetX: offsetX, offsetY: offsetY)
+    }
+
+    /// Computes the sum of two vectors, scaling each one separately (single-precision).
+    /// On return, the contents of vector Y are replaced with the result.
+    /// https://developer.apple.com/documentation/accelerate/blas
+    /// - Parameters:
+    ///     - n: number of items
+    ///     - alpha: scaling factor x
+    ///     - incX: stride within x
+    ///     - beta: scaling factor for y
+    ///     - incY: stride within y
+    ///     - offsetX: offset w.r.t to the start of the array from which to start the operation in x
+    ///     - offsetY: offset w.r.t to the start of the array from which to start the operation in y
+    mutating func axpby(n: Int32, alpha: Element, incX: Int32 = 1, beta: Element, y: inout [Element], incY: Int32 = 1, offsetX: Int = 0, offsetY: Int = 0) {
+        assert(offsetX == 0 || (offsetX >= 0 && offsetX < count),
+            "\(offsetX) == 0 || (\(offsetX) >= 0 && \(offsetX) < \(count))")
+        assert(offsetY == 0 || (offsetY >= 0 && offsetY < y.count),
+            "\(offsetY) == 0 || (\(offsetY) >= 0 && \(offsetY) < \(y.count))")
+        assert(n >= 0 && n / incX <= count - offsetX, "\(n) >= 0 && \(n) / \(incX) <= \(count) - \(offsetX)")
+        assert(n >= 0 && n / incY <= y.count - offsetY, "\(n) >= 0 && \(n) / \(incY) <= \(y.count) - \(offsetY)")
+        assert(incX >= 1, "\(incX) >= 1")
+        assert(incY >= 1, "\(incY) >= 1")
+        catlas_saxpby(n, alpha, &self + offsetX, incX, beta, &y + offsetY, incY)
     }
 
 }
@@ -68,6 +109,12 @@ public extension Array where Element == Double {
 
     /// Modifies a vector in place, setting each element to a given value.
     /// https://developer.apple.com/documentation/accelerate/blas
+    ///
+    /// - Parameters:
+    ///     - n: number of items
+    ///     - alpha: scaling factor
+    ///     - incX: stride within x
+    ///     - offset: offset w.r.t to the start of the array from which to start the operation
     mutating func set(n: Int32, alpha: Element, incX: Int32 = 1, offset: Int = 0) {
         assert(offset == 0 || (offset >= 0 && offset < count),
             "\(offset) == 0 || (\(offset) >= 0 && \(offset) < \(count))")
@@ -75,5 +122,40 @@ public extension Array where Element == Double {
             "\(n) >= 0 && \(n) <= \(count) - \(offset)")
         assert(incX >= 1, "\(incX) >= 1")
         catlas_dset(n, alpha, &self + offset, incX)
+    }
+
+    /// Computes the sum of two vectors, scaling each one separately (single-precision).
+    /// On return, the contents of vector Y are replaced with the result.
+    /// https://developer.apple.com/documentation/accelerate/blas
+    /// - Parameters:
+    ///     - alpha: scaling factor x
+    ///     - beta: scaling factor for y
+    ///     - offsetX: offset w.r.t to the start of the array from which to start the operation in x
+    ///     - offsetY: offset w.r.t to the start of the array from which to start the operation in y
+    mutating func axpby(alpha: Element, beta: Element, y: inout [Element], offsetX: Int = 0, offsetY: Int = 0) {
+        axpby(n: Int32(count - offsetX), alpha: alpha, beta: beta, y: &y, offsetX: offsetX, offsetY: offsetY)
+    }
+
+    /// Computes the sum of two vectors, scaling each one separately (single-precision).
+    /// On return, the contents of vector Y are replaced with the result.
+    /// https://developer.apple.com/documentation/accelerate/blas
+    /// - Parameters:
+    ///     - n: number of items
+    ///     - alpha: scaling factor x
+    ///     - incX: stride within x
+    ///     - beta: scaling factor for y
+    ///     - incY: stride within y
+    ///     - offsetX: offset w.r.t to the start of the array from which to start the operation in x
+    ///     - offsetY: offset w.r.t to the start of the array from which to start the operation in y
+    mutating func axpby(n: Int32, alpha: Element, incX: Int32 = 1, beta: Element, y: inout [Element], incY: Int32 = 1, offsetX: Int = 0, offsetY: Int = 0) {
+        assert(offsetX == 0 || (offsetX >= 0 && offsetX < count),
+            "\(offsetX) == 0 || (\(offsetX) >= 0 && \(offsetX) < \(count))")
+        assert(offsetY == 0 || (offsetY >= 0 && offsetY < y.count),
+            "\(offsetY) == 0 || (\(offsetY) >= 0 && \(offsetY) < \(y.count))")
+        assert(n >= 0 && n / incX <= count - offsetX, "\(n) >= 0 && \(n) / \(incX) <= \(count) - \(offsetX)")
+        assert(n >= 0 && n / incY <= y.count - offsetY, "\(n) >= 0 && \(n) / \(incY) <= \(y.count) - \(offsetY)")
+        assert(incX >= 1, "\(incX) >= 1")
+        assert(incY >= 1, "\(incY) >= 1")
+        catlas_daxpby(n, alpha, &self + offsetX, incX, beta, &y + offsetY, incY)
     }
 }
